94cd3f5e6920f34656a253aea3c7420d43599ea2
Marco Campominosi <marco.campominosi@gmail.com>
2023-08-20 18:14:15 +0200

    endPoints

---
+++ client-server/server-side/README.md
@@ -0,0 +1,30 @@
+# Server-side implementation with Python
+Starting from "Hymenoptera Recognizer" project, this fork add a possible server-side implementation to allow users to ask prediction for an image
+
+## Pre-Trained Model
+In this example we use original [pre-trained model](/Hymenoptera-Recognizer/models/yolov5m_v2.pt).
+
+* ### [yolov5m_v2 (from original documentation)](../README.md#yolov5m_v2)
+
+## Configuration to run `endPoint.py` on Ubuntu server:
+
+### First of all read and install [yolov5 project](https://github.com/ultralytics/yolov5): follow [pypi.org_yolov5](https://pypi.org/project/yolov5/)
+```bash
+pip install yolov5
+```
+
+### Install [FLASK](https://flask.palletsprojects.com/en/2.2.x/)
+```bash
+pip install Flask
+```
+
+### Install SCREEN
+```bash
+pip install screen
+```
+
+## Now you can run the below code from the folder where you saved `endPoint.py` and Machine Learning Model:
+```bash
+screen
+flask --app endPoint run --host=0.0.0.0 --port 5001
+```
---
+++ client-server/server-side/endPoint.py
@@ -0,0 +1,88 @@
+import yolov5
+
+# First of all load pre-trained model 'Hymenoptera-Recognizer'
+print('Model Loading...')
+model = yolov5.load('./model/yolov5m_v2.pt')
+print('Model Loaded!')
+  
+# Set model parameters
+model.conf = 0.25  # NMS confidence threshold
+model.iou = 0.45  # NMS IoU threshold
+model.agnostic = False  # NMS class-agnostic
+model.multi_label = False  # NMS multiple labels per box
+model.max_det = 20  # maximum number of detections per image
+
+
+from flask import Flask, request
+import shutil
+import base64
+import json
+
+counter = -1
+
+myEndPoint = Flask(__name__)
+
+@myEndPoint.route("/api/v2/test", methods = ['GET'])
+def test():
+    return {"status": "Service Up!"}, 200
+
+@myEndPoint.route("/api/v2/img/inference", methods = ['POST'])
+def predict_image():
+    global counter
+
+    # Check if the post request has the file part
+    if 'userImage' not in request.files:
+        return {"error": "NO FILE"}, 415
+
+    # Get image
+    img = request.files['userImage']
+    img.save("inputImage")
+
+    # Perform inference
+    yoloPrediction = model("inputImage")
+
+    # Parse results
+    predictions = yoloPrediction.pred[0]
+    # boxes = predictions[:, :4] # x1, y1, x2, y2
+    scores = predictions[:, 4]
+    categories = predictions[:, 5]
+
+    #Dictionary for categories found
+    outputDictionary = {}
+
+    #Analysis of predictions
+    for index, cat in enumerate(categories):
+        type = ""
+        
+        if 0. in cat:
+            type = "Ape"                                #red
+        if 1. in cat:
+            type = "Vespa"                              #pink
+        if 2. in cat:
+            type = "Calabrone Europeo"                  #orange
+        if 3. in cat:
+            type = "Vespa Orientale"                    #light orange
+        if 4. in cat:
+            type = "Velutina (Calabrone Asiatico)"      #yellow
+
+        outputDictionary[str(index)] = type + "_" + str(scores[index])[7:14]   
+
+    #yoloPrediction.show()
+
+    image_file = 'responseImage.jpg'
+    # Save results
+    yoloPrediction.save(image_file)
+
+    #Bytes convertion
+    with open(image_file, "rb") as f:
+        im_bytes = f.read()        
+    im_b64 = base64.b64encode(im_bytes).decode("utf8")
+
+    #Delete export folder
+    shutil.rmtree("./runs")
+
+    counter = counter + 1 
+
+    return json.dumps({'counter' : counter, 'analysis': outputDictionary, 'image': im_b64}), 200
+
+print('App loaded!')
